3629a05355116c70d0b0c0b71ee7341f5d703af2
NyeinMinSoepu <nyeinminsoe@parami.edu.mm>
2025-12-24 03:52:58 +0630

    Add files via upload

---
+++ bank_kmeans_web.py
@@ -0,0 +1,100 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import joblib
+
+
+model = joblib.load('bank_kmeans.pkl')
+
+
+col_to_drop = ['y', 'default']
+df = pd.read_csv("bank.csv", sep=";").drop(columns=col_to_drop, axis=1)
+
+
+labels = model.named_steps['kmeans'].labels_
+df_clustered = df.copy()
+df_clustered['cluster'] = labels
+    
+num_cols = df.select_dtypes(include=['int64']).columns.tolist()
+cat_cols = df.select_dtypes(include=['object']).columns.tolist()
+
+# calculate mean and mode
+summary = df_clustered.groupby('cluster').agg({
+        **{col: 'mean' for col in num_cols},
+        **{col: lambda x: x.mode()[0] if not x.mode().empty else "N/A" for col in cat_cols}
+    }).reset_index()
+
+# title
+st.title("Cluster Details Dashboard")
+st.markdown("Detailed breakdown of clusters resulted from the model.")
+
+# Select Cluster
+st.sidebar.header("Filter Options")
+selected_cluster = st.sidebar.selectbox("Select a Cluster to Inspect", sorted(df_clustered['cluster'].unique()))
+
+
+st.metric("Total Customers in Cluster", len(df_clustered[df_clustered['cluster'] == selected_cluster]))
+
+# Cluster Profile
+col1, col2 = st.columns(2)
+
+
+with col1:
+    st.subheader("Categorical 'Mode'")
+    # Show categorical modes for the selected cluster
+    cluster_modes = df_clustered[df_clustered['cluster'] == selected_cluster].select_dtypes(include='object').mode().T
+    cluster_modes.columns = ['Most Frequent Value']
+    st.table(cluster_modes)
+
+with col2:
+    st.subheader("Numerical Averages")
+    # Show numerical averages for the selected cluster
+    cluster_avgs = df_clustered[df_clustered['cluster'] == selected_cluster].select_dtypes(include='number').drop(columns='cluster').mean().T
+    cluster_avgs.columns = ['Average Value']
+    st.table(cluster_avgs)
+
+
+# plot
+st.subheader("Cluster Distribution Plot")
+cluster_df = df_clustered[df_clustered['cluster'] == selected_cluster]
+
+# tick
+show_full_data = st.checkbox("Show other clusters in background", value=True)
+
+plot_cols = [c for c in df.columns if c != 'cluster']
+x_axis = st.selectbox("Select X-axis", plot_cols, index=0)
+y_axis = st.selectbox("Select Y-axis", plot_cols, index=2)
+
+
+
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# background
+sns.scatterplot(
+    data=df_clustered, 
+    x=x_axis, 
+    y=y_axis, 
+    color='green', 
+    alpha=0.3, 
+    ax=ax, 
+    label='Other Clusters'
+)
+
+# show cluster
+cluster_df = df_clustered[df_clustered['cluster'] == selected_cluster]
+
+sns.scatterplot(
+    data=cluster_df, 
+    x=x_axis, 
+    y=y_axis, 
+    color='green',
+    s=90,
+    edgecolor='black', 
+    ax=ax, 
+    label=f"Cluster {selected_cluster}"
+)
+
+plt.title(f"Cluster {selected_cluster}: {x_axis} vs {y_axis}")
+plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left') 
+st.pyplot(fig)
